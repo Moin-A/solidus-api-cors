4779f80f37b8cd7e1d8370e95e0f7f17d4c55809
Apple <apple@Apples-MacBook-Pro.local>
2025-11-20 02:38:48 +0530

    Fix Zeitwerk NameError, remove binding.pry, fix Puma double bind

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ end
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Bind to all interfaces (0.0.0.0) so the app is accessible from outside the container
 bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"

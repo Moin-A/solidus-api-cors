8307525e28ce9454c3b38540cb176828b8bf395a
Apple <apple@Apples-MacBook-Pro.local>
2025-11-20 13:47:51 +0530

    Add spree/backend/themes/solidus_admin.css to assets precompile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += ['solidus_starter_frontend_manifest.js']
+Rails.application.config.assets.precompile += ['spree/backend/themes/solidus_admin.css']
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'fonts')

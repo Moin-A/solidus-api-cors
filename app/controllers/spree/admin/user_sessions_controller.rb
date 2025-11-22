f2cbb0958932537f4a071636d9f9c137b0208dc8
Apple <apple@Apples-MacBook-Pro.local>
2025-11-22 14:10:53 +0530

    added a user_session_controller in admin namespace

---
+++ app/controllers/spree/admin/user_sessions_controller.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+# Override the admin user sessions controller from solidus_auth_devise
+# Based on: solidus_auth_devise/lib/controllers/backend/spree/admin/user_sessions_controller.rb
+class Spree::Admin::UserSessionsController < Devise::SessionsController
+  helper 'spree/base'
+
+  include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::Store
+
+  helper 'spree/admin/navigation'
+  layout 'spree/layouts/admin'
+
+  # Completely disable CSRF protection for this controller
+  # This is needed because the admin login form may not have a valid CSRF token
+  # in production environments (e.g., when behind Traefik reverse proxy)
+  skip_forgery_protection
+
+  # Ensure we skip any authentication checks that might come from concerns
+
+  # Override verify_authenticity_token to do nothing
+  def create
+    authenticate_spree_user!
+
+    if spree_user_signed_in?
+      respond_to do |format|
+        format.html {
+          flash[:success] = I18n.t('spree.logged_in_succesfully')
+          redirect_back_or_default(after_sign_in_path_for(spree_current_user))
+        }
+        format.js {
+          user = resource.record
+          render json: { ship_address: user.ship_address, bill_address: user.bill_address }.to_json
+        }
+      end
+    else
+      flash.now[:error] = t('devise.failure.invalid')
+      render :new
+    end
+  end
+
+  def authorization_failure
+  end
+
+  private
+
+  def signed_in_root_path(_resource)
+    spree.admin_path
+  end
+
+  # NOTE: as soon as this gem stops supporting Solidus 3.1 if-else should be removed and left only include
+  if defined?(::Spree::Admin::SetsUserLanguageLocaleKey)
+    include ::Spree::Admin::SetsUserLanguageLocaleKey
+  else
+    def set_user_language_locale_key
+      :admin_locale
+    end
+  end
+
+  def accurate_title
+    I18n.t('spree.login')
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session["spree_user_return_to"] || default)
+    session["spree_user_return_to"] = nil
+  end
+end

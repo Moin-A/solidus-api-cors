45f515542efe77509c342cf284962279557364e4
Apple <apple@Apples-MacBook-Pro.local>
2025-12-05 01:47:38 +0530

    minot fixs

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,5 +1,5 @@
 Spree::User.class_eval do
   devise :confirmable unless devise_modules.include?(:confirmable)
-  has_many :product_rating, foreign_key: "user_id", class_name: "Spree::User", inverse_of: :user
+  has_many :product_ratings, foreign_key: "user_id", class_name: "Spree::Rating", inverse_of: :user
   include UserVerification
 end
